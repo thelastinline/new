3ba2a1439c9b57a935f535f710a82f5e327a9a60
Juan Plascencia <juan.plascencia_@hotmail.com>
2019-06-03 08:03:22 -0500

    se cambio el titulo de la pagina

--- 1.php
+++ 1.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DESCARGAS</title>
+    <title>caca</title>
     <link rel="icon" type="image/png" href="images/favicon.png">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/estilos.css">
